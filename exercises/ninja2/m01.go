eac3cfa421b2f2d70e21388dd61d946abe5340bf
Greg Stilwell <greg.stilwell@gmail.com>
2019-09-14 13:01:12 -0400

    moving stuff

--- exercises/ninja2/m01.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    x := 42
-    fmt.Printf("decimal: %d\nbinary: %b\nhex: %#x\n", x,x,x)
-}
--- exercises/ninja3/ex01.go
+++ exercises/ninja3/ex01.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
-func main {
-  
+func main() {
+	for i := 1; i <= 1000; i++ {
+		fmt.Printf("%d ", i)
+	}
+	fmt.Println()
 }
