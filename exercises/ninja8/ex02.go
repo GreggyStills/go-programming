07601c58d1e0c961997b935e65a64c73e99b4d55
Greg Stilwell <greg.stilwell@gmail.com>
2019-11-03 16:55:34 -0500

    ninja8, ex02.go

---
+++ exercises/ninja8/ex02.go
@@ -0,0 +1,26 @@
+/*
+Starting with this code ( https://play.golang.org/p/b_UuCcZag9 ),
+unmarshal the JSON into a Go data structure.
+*/
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
+}
+
+func main() {
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
+	fmt.Println(s)
+
+	var people []Person
+	json.Unmarshal([]byte(s), &people)
+	fmt.Println(people)
+}
