e991f92248c5b978871840c5c9c501fec2acc6b6
Greg Stilwell <greg.stilwell@gmail.com>
2019-11-03 16:33:46 -0500

    ninja8, ex01.go

--- exercises/ninja8/ex01.go
+++ exercises/ninja8/ex01.go
@@ -1,3 +1,6 @@
+/*
+ Starting with this code ( https://play.golang.org/p/_fQUGm9Utvl ), marshal the []user to JSON.
+*/
 package main
 
 import (
@@ -26,10 +29,10 @@ func main() {
 	users := []user{u1, u2, u3}
 	fmt.Println(users)
 
+	// your code goes here
 	jsonUsers, err := json.Marshal(users)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(string(jsonUsers))
-
 }
