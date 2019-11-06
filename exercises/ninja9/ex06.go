2bf2b28234fd8b5a622030a6da034c112f755658
Greg Stilwell <greg.stilwell@gmail.com>
2019-11-06 18:38:05 -0500

    ninja9, ex06.go

---
+++ exercises/ninja9/ex06.go
@@ -0,0 +1,16 @@
+/*
+Create a program that prints out your OS and ARCH. Use the following commands to run it
+- go run
+- go build
+- go install
+*/
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Printf("OS:\t%s\nCPU:\t%s\n", runtime.GOOS, runtime.GOARCH)
+}
