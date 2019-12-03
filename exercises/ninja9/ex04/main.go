cd2e2e4abf90b4c0d0bd55463da10b831357ac8c
gstilwell <gstilwell@salesforce.com>
2019-12-03 16:57:53 -0500

    ninja9, ex04

--- exercises/ninja9/ex04/main.go
+++ exercises/ninja9/ex04/main.go
@@ -2,9 +2,34 @@
 Fix the race condition you created in the previous exercise by using a mutex
 it makes sense to remove runtime.Gosched()
 */
-
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+var counter int
+var mu sync.Mutex
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	for i := 0; i < 101; i++ {
+		go increment()
+
+	}
+}
+
+func increment() {
+	mu.Lock()
+	if counter%5 == 0 {
+		fmt.Println()
+	} else {
+		fmt.Printf("\t")
+	}
+	counter++
+	fmt.Printf("%d (gr: %d)", counter, runtime.NumGoroutine())
+	mu.Unlock()
 }
