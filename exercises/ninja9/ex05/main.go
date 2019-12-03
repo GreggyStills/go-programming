9d08561d9fc9cf9b5cb3bea363ea65b4a88b0f9d
gstilwell <gstilwell@salesforce.com>
2019-12-03 17:22:51 -0500

    ninja9, ex05

--- exercises/ninja9/ex05/main.go
+++ exercises/ninja9/ex05/main.go
@@ -1,9 +1,36 @@
 /*
-Fix the race condition you created in exercise #4 by using package atomic
+Fix the race condition you created in exercise #3 by using package atomic
 */
-
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
 
+var counter int64
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	iters := 101
+	wg.Add(iters)
+
+	for i := 0; i < iters; i++ {
+		go increment()
+	}
+	wg.Wait()
+}
+
+func increment() {
+	atomic.AddInt64(&counter, 1)
+	if atomic.LoadInt64(&counter)%5 == 0 {
+		fmt.Println()
+	}
+	fmt.Printf("%d (gr: %d)\t", atomic.LoadInt64(&counter), runtime.NumGoroutine())
+	wg.Done()
 }
